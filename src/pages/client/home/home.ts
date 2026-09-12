5c1d940201fc78c048098bf50703f8d868803388
Nicolas Tempio <nicotempio93@gmail.com>
2026-09-12 20:13:36 +0200

    feat(H2-P1-03): agregar productos al carrito

--- src/pages/client/home/home.ts
+++ src/pages/client/home/home.ts
@@ -1,19 +1,31 @@
-import { checkAuhtUser, logout } from "../../../utils/auth";
+import { PRODUCTS } from "../../../data/data.ts";
+import type { IProduct } from "../../../types/product.ts";
+import { addCartItem } from "../../../utils/cart.ts";
 
-const buttonLogout = document.getElementById(
-  "logoutButton"
-) as HTMLButtonElement;
-buttonLogout?.addEventListener("click", () => {
-  logout();
-});
+const renderizarProductos = (productos: IProduct[]) => {
+  const contenedorProductos = document.getElementById("contenedor-productos");
 
+  productos.forEach((producto) => {
+    const { id, nombre, precio } = producto;
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+      <img class="producto-imagen" src="../../../../public/pizza.jpg" alt="${nombre}">
+      <div class="producto-detalles">
+        <h3 class="producto-nombre">${nombre}</h3>
+        <p class="producto-precio">$${precio}</p>
+        <button class="producto-boton" data-id="${id}">Agregar al carrito</button>
+      </div>
+    `;
 
-const initPage = () => {
-  console.log("inicio de pagina");
-  checkAuhtUser(
-    "/src/pages/auth/login/login.html",
-    "/src/pages/admin/home/home.html",
-    "client"
-  );
+    const botonAgregar = div.querySelector(".producto-boton");
+    botonAgregar?.addEventListener("click", () => {
+      addCartItem(producto);
+    });
+    contenedorProductos?.appendChild(div);
+  });
 };
-initPage();
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderizarProductos(PRODUCTS);
+});
