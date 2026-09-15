6a27833f79547a5eb8e332737812b77ee8b75d5e
Nicolas Tempio <nicotempio93@gmail.com>
2026-09-15 18:42:36 +0200

    Agregamos funcionalidad de busqueda por input, y agregamos tambien filtrado por busqueda en boton de categorias del sidebar

--- src/pages/client/home/home.ts
+++ src/pages/client/home/home.ts
@@ -1,15 +1,29 @@
 import { PRODUCTS } from "../../../data/data.ts";
 import type { IProduct } from "../../../types/product.ts";
 import { addCartItem } from "../../../utils/cart.ts";
+import { getCategories } from "../../../data/data.ts";
+
+const inputBusqueda = document.getElementById(
+  "inputBusqueda",
+) as HTMLInputElement;
+inputBusqueda.addEventListener("input", () => {
+  const busqueda = inputBusqueda.value.toLowerCase();
+  const productosFiltrados = PRODUCTS.filter((producto) =>
+    producto.nombre.toLowerCase().includes(busqueda),
+  );
+  renderizarProductos(productosFiltrados);
+});
 
 const renderizarProductos = (productos: IProduct[]) => {
   const contenedorProductos = document.getElementById("contenedor-productos");
-
-  productos.forEach((producto) => {
-    const { id, nombre, precio } = producto;
-    const div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `
+  if (!contenedorProductos) return;
+  contenedorProductos.innerHTML = "";
+  if (productos.length !== 0) {
+    productos.forEach((producto) => {
+      const { id, nombre, precio } = producto;
+      const div = document.createElement("div");
+      div.classList.add("producto");
+      div.innerHTML = `
       <img class="producto-imagen" src="../../../../public/pizza.jpg" alt="${nombre}">
       <div class="producto-detalles">
         <h3 class="producto-nombre">${nombre}</h3>
@@ -18,13 +32,44 @@ const renderizarProductos = (productos: IProduct[]) => {
       </div>
     `;
 
-    const botonAgregar = div.querySelector(".producto-boton");
-    botonAgregar?.addEventListener("click", () => {
-      addCartItem(producto);
+      const botonAgregar = div.querySelector(".producto-boton");
+      botonAgregar?.addEventListener("click", () => {
+        addCartItem(producto);
+      });
+      contenedorProductos?.appendChild(div);
     });
-    contenedorProductos?.appendChild(div);
-  });
+  } else {
+    contenedorProductos.innerHTML = "<p>No hay productos disponibles.</p>";
+  }
 };
+
+const filtrarPorCategoria = (categoria: string) => {
+  const productosFiltrados = PRODUCTS.filter((producto) =>
+    producto.categorias.some((cat) => cat.nombre === categoria),
+  );
+  renderizarProductos(productosFiltrados);
+};
+
+const contenedorCategorias = document.getElementById("contenedor-categorias");
+
+const btnTodos = document.createElement("button");
+btnTodos.classList.add("categoria-boton");
+btnTodos.textContent = "Todos";
+contenedorCategorias?.appendChild(btnTodos);
+btnTodos.addEventListener("click", () => {
+  renderizarProductos(PRODUCTS);
+});
+
+getCategories().forEach((categoria) => {
+  const { nombre } = categoria;
+  const btnCat = document.createElement("button");
+  btnCat.classList.add("categoria-boton");
+  btnCat.textContent = nombre;
+  contenedorCategorias?.appendChild(btnCat);
+  btnCat.addEventListener("click", () => {
+    filtrarPorCategoria(nombre);
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   renderizarProductos(PRODUCTS);
