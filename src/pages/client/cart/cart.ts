866a16f4381a3c95720c4ccbabf7768e62d28658
Nicolas Tempio <nicotempio93@gmail.com>
2026-09-15 20:11:22 +0200

    Ajustes en el css, de diseño para que se vea mejor

--- src/pages/client/cart/cart.ts
+++ src/pages/client/cart/cart.ts
@@ -1,4 +1,4 @@
-import { getCartItems } from "../../../utils/cart.ts";
+import { getCartItems, getCartTotal } from "../../../utils/cart.ts";
 
 const contenedorCarrito = document.getElementById("contenedor-carrito");
 const totalCarrito = document.getElementById("total-carrito");
@@ -8,28 +8,22 @@ const renderizarCarrito = () => {
   if (!contenedorCarrito || !totalCarrito) return;
   contenedorCarrito.innerHTML = "";
   if (carrito.length !== 0) {
-    carrito.forEach(
-      (producto: { nombre: string; precio: number; cantidad: number }) => {
-        const { nombre, precio, cantidad } = producto;
-        const div = document.createElement("div");
-        div.classList.add("producto-carrito");
-        div.innerHTML = `
+    carrito.forEach((producto) => {
+      const { nombre, precio, cantidad } = producto;
+      const div = document.createElement("div");
+      div.classList.add("producto-carrito");
+      div.innerHTML = `
                 <h3 class="producto-nombre">${nombre}</h3>
                 <p class="producto-precio">$${precio}</p>
                 <p class="producto-cantidad">Cantidad: ${cantidad}</p>
                 <span class="producto-subtotal">Subtotal: $${precio * cantidad}</span>
             `;
-        contenedorCarrito?.appendChild(div);
-      },
-    );
+      contenedorCarrito?.appendChild(div);
+    });
   } else {
     contenedorCarrito.innerHTML = "<p>El carrito está vacío.</p>";
   }
-  const total = carrito.reduce(
-    (total: number, item: { precio: number; cantidad: number }) =>
-      total + item.precio * item.cantidad,
-    0,
-  );
+  const total = getCartTotal();
   totalCarrito.textContent = `Total: $${total}`;
 };
 
